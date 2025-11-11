7af5436e7f6a08f64e1bcd030f07483c54c0ba22
SuhailTalpur <suhailbaloch2000@gmail.com>
2025-11-11 12:58:57 +0500

    icon and name changes2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.png'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProjectDetail from "./components/ProjectDetail";
